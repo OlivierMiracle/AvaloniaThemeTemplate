47310a9e300b48a511c072618ebf0ca886ff8abc
OliverMiracle <adam.cisz@pm.me>
2024-07-25 08:19:12 +0200

    Adds template;

---
+++ AvaloniaThemeTemplate/AvaloniaThemeTemplate.axaml.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+
+namespace AvaloniaThemeTemplate;
+
+public class AvaloniaThemeTemplate : Styles
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AvaloniaThemeTemplate"/> class.
+    /// </summary>
+    /// <param name="sp">The parent's service provider.</param>
+    public AvaloniaThemeTemplate(IServiceProvider? sp = null)
+    {
+        AvaloniaXamlLoader.Load(sp, this);
+    }
+}
